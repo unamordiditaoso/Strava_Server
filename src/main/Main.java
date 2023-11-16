6bb82c4b629f240548f6e2505e3f8c806db42e9b
etxan <etxan@192.168.1.143>
2023-11-16 15:58:42 +0100

    conexion con cliente

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.rmi.Naming;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -12,10 +13,35 @@ import data.domain.Reto;
 import data.domain.TipoRegistro;
 import data.domain.TipoReto;
 import data.domain.Usuario;
+import remote.IRemoteFacade;
+import remote.RemoteFacade;
 
 public class Main {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+		
+		//Activate Security Manager. It is needed for RMI.
+//				if (System.getSecurityManager() == null) {
+//					System.setSecurityManager(new SecurityManager());
+//				}
+
+		//args[0] = RMIRegistry IP
+		//args[1] = RMIRegistry Port
+		//args[2] = Service Name
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];		
+			
+		//Bind remote facade instance to a sirvice name using RMIRegistry
+		try {
+			IRemoteFacade remoteFacade = new RemoteFacade();			
+			Naming.rebind(name, remoteFacade);
+			System.out.println(" * Strava Server v1 '" + name + "' started!!");
+		} catch (Exception ex) {
+			System.err.println(" # Strava Server Exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+		
+		String serverName = System.getProperty("server.name");
+		System.out.println("Server Name: " + serverName);
 		Date d = new Date(2003, 04, 27);
 		
 		Date d1 = new Date(2023, 11, 12, 12, 20);
